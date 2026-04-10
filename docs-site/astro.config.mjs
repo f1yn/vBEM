111529860da47b3165e8f143931fb9f9955e5e97
Flynn <gpg@flyn.ca>
2026-04-09 21:10:00 -0400

    (site) SEO and sitemap integaration for docs. More updates

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -1,11 +1,14 @@
 import path from "node:path";
 import { defineConfig, fontProviders } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 const dirname = path.resolve(import.meta.dirname);
 const sassPreloadLocation = path.join(dirname, "./src/styles/preload.scss");
 
 export default defineConfig({
-	integrations: [],
+	site: "https://vbem.dev",
+	integrations: [sitemap()],
 	fonts: [
 		{
 			provider: fontProviders.fontsource(),
